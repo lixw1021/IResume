755e97f622e56c9c76cd51979d7a38f8d1440047
xianwei li <xil148@pitt.edu>
2017-03-28 09:52:09 -0400

    fake tow education data and separate education items from main activity

--- app/src/main/java/com/example/xianweili/myapplication/MainActivity.java
+++ app/src/main/java/com/example/xianweili/myapplication/MainActivity.java
@@ -21,54 +21,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int REQ_CODE_EDIT_EDUCATION = 100;
     private BasicInfo basicInfo;
     private List<Education> educations;
-
-    private static final String MODEL_EDUCATIONS = "educations";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        loadData();
+        fakeData();
         setupUI();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == REQ_CODE_EDUCATION_EDIT && resultCode == Activity.RESULT_OK) {
-//            Education education = data.getParcelableExtra(EducationEditActivity.KEY_EDUCATION);
-//            educations.add(education);
-//            setupEducations();
-//        }
-        if (resultCode == Activity.RESULT_OK) {
-            switch (requestCode) {
-                case REQ_CODE_EDIT_EDUCATION:
-                    String educationId = data.getStringExtra(EducationEditActivity.KEY_EDUCATION_ID);
-                    if (educationId != null) {
-                        deleteEducation(educationId);
-                    } else {
-                        Education education = data.getParcelableExtra(EducationEditActivity.KEY_EDUCATION);
-                        updateEducation(education);
-                    }
-                    break;
-            }
-        }
     }
 
     private void setupUI() {
         setContentView(R.layout.activity_main);
-        ImageButton addEducationBtn = (ImageButton) findViewById(R.id.add_education_btn);
-        addEducationBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EducationEditActivity.class);
-                startActivityForResult(intent,REQ_CODE_EDIT_EDUCATION);
-            }
-        });
-        //setupBasicInfoUI();
+
+        setupBasicInfoUI();
         setupEducations();
     }
 
@@ -79,50 +46,17 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupEducations(){
         LinearLayout educationsLayout = (LinearLayout) findViewById(R.id.education_list);
-        educationsLayout.removeAllViews();
-        for (Education education :educations){
+        for (Education education: educations){
             educationsLayout.addView(getEducationView(education));
         }
     }
 
-    private View getEducationView(final Education education){
-        View view = getLayoutInflater().inflate(R.layout.education_list,null);
-        String dateString = DateUtils.dateToString(education.startDate) +"-"+DateUtils.dateToString(education.endDate);
-        ((TextView) view.findViewById(R.id.education_school)).setText(education.school + "(" + dateString + ")");
+    private View getEducationView(Education education) {
+        View view = getLayoutInflater().inflate(R.layout.education_list, null);
+        String dateString = DateUtils.dateToString(education.startDate) + " ~ " + DateUtils.dateToString(education.endDate);
+        ((TextView) view.findViewById(R.id.education_school)).setText(education.school + " (" + dateString + ")");
         ((TextView) view.findViewById(R.id.education_courses)).setText(formatItems(education.courses));
-
-        view.findViewById(R.id.edit_education_btn).setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EducationEditActivity.class);
-                intent.putExtra(EducationEditActivity.KEY_EDUCATION,education);
-                startActivityForResult(intent,REQ_CODE_EDIT_EDUCATION);
-            }
-        });
         return view;
-    }
-
-
-    private void loadData() {
-//        BasicInfo savedBasicInfo = ModelUtils.read(this,
-//                MODEL_BASIC_INFO,
-//                new TypeToken<BasicInfo>(){});
-//        basicInfo = savedBasicInfo == null ? new BasicInfo() : savedBasicInfo;
-
-        List<Education> savedEducation = ModelUtils.read(this,
-                MODEL_EDUCATIONS,
-                new TypeToken<List<Education>>(){});
-        educations = savedEducation == null ? new ArrayList<Education>() : savedEducation;
-
-//        List<Experience> savedExperience = ModelUtils.read(this,
-//                MODEL_EXPERIENCES,
-//                new TypeToken<List<Experience>>(){});
-//        experiences = savedExperience == null ? new ArrayList<Experience>() : savedExperience;
-//
-//        List<Project> savedProjects = ModelUtils.read(this,
-//                MODEL_PROJECTS,
-//                new TypeToken<List<Project>>(){});
-//        projects = savedProjects == null ? new ArrayList<Project>() : savedProjects;
     }
 
     public static String formatItems(List<String> items) {
@@ -136,35 +70,32 @@ public class MainActivity extends AppCompatActivity {
         return sb.toString();
     }
 
-    private void updateEducation(Education education) {
-        boolean found = false;
-        for (int i = 0; i < educations.size(); ++i) {
-            Education e = educations.get(i);
-            if (TextUtils.equals(e.id, education.id)) {
-                found = true;
-                educations.set(i, education);
-                break;
-            }
-        }
+    private void fakeData() {
+        basicInfo = new BasicInfo();
+        basicInfo.name = "xianwei li";
+        basicInfo.email = "abc@gmail.com";
 
-        if (!found) {
-            educations.add(education);
-        }
+        Education education1 = new Education();
+        education1.school = "pitt";
+        education1.major = "ME";
+        education1.startDate = DateUtils.stringToDate("09/2014");
+        education1.endDate = DateUtils.stringToDate("4/2016");
+        education1.courses = new ArrayList<>();
+        education1.courses.add("Database");
+        education1.courses.add("Algorithms");
+        education1.courses.add("Networks");
 
-        ModelUtils.save(this, MODEL_EDUCATIONS, educations);
-        setupEducations();
-    }
+        Education education2 = new Education();
+        education2.school = "NCEPU";
+        education2.major = "IS";
+        education2.startDate = DateUtils.stringToDate("09/2010");
+        education2.endDate = DateUtils.stringToDate("4/2014");
+        education2.courses = new ArrayList<>();
+        education2.courses.add("information");
 
-    private void deleteEducation(@NonNull String educationId) {
-        for (int i = 0; i < educations.size(); ++i) {
-            Education e = educations.get(i);
-            if (TextUtils.equals(e.id, educationId)) {
-                educations.remove(i);
-                break;
-            }
-        }
+        educations = new ArrayList<>();
+        educations.add(education1);
+        educations.add(education2);
 
-        ModelUtils.save(this, MODEL_EDUCATIONS, educations);
-        setupEducations();
     }
 }
